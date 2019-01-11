73f159e2f65fc494b7657a9847c0850ab52d44a5
Deepak Lalwani <dlalwani444@gmail.com>
2019-01-12 02:40:39 +0530

    Updates

---
+++ precache-manifest.354d6de01d7c59b2a91e1c38dcfec0b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "66c1dd94144ce888f31f",
+    "url": "/BooksSearch/static/css/main.f4a9561c.chunk.css"
+  },
+  {
+    "revision": "66c1dd94144ce888f31f",
+    "url": "/BooksSearch/static/js/main.66c1dd94.chunk.js"
+  },
+  {
+    "revision": "5015f8266d5832c8819a",
+    "url": "/BooksSearch/static/js/1.5015f826.chunk.js"
+  },
+  {
+    "revision": "1fb26c1a79c765006a4e",
+    "url": "/BooksSearch/static/js/runtime~main.1fb26c1a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/BooksSearch/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "53f46638bf981d15394b478a3505e1a0",
+    "url": "/BooksSearch/index.html"
+  }
+];
